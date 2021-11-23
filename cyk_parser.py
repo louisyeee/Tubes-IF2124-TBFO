4cc428eed2ca4f89667c307f403606e288c145f6
averrous-s <16520480@std.stei.itb.ac.id>
2021-11-23 11:22:52 +0700

    Add tested cyk

--- cyk_parser.py
+++ cyk_parser.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from tabulate import tabulate
 
 '''
 Asumsi:
@@ -7,7 +7,12 @@ Asumsi:
 - CNF dalam bentuk dictionary
 '''
 
+def beautiful_print(matrix):
+    headers = [f'col{i}' for i in range(len(matrix))]
 
+    table = tabulate(matrix, headers, tablefmt="fancy_grid")
+
+    print(table)
 
 def is_product(CNF, parent,  input1, input2 = None):
     if (input2 == None):
@@ -27,8 +32,8 @@ def nearest_non_terminal(CNF, input1, input2 = None):
     return None
 
 def fill_diagonal_matrix_with_nearest_non_terminal(CNF, matrix, text):
-    for i in len(matrix):
-        if (matrix[i][i] == 0):
+    for i in range(len(matrix)):
+        if (matrix[i][i] == 'NULL'):
             matrix[i][i] = nearest_non_terminal(CNF, text[i])
 
 def is_grammar_fulfilled_in_cyk_matrix(start_state, cyk_matrix):
@@ -40,16 +45,24 @@ def process_diagonal_matrix(CNF, matrix, diagonalKe):
     pointer = 0
     # baris -> i + diagonalKe
     while (pointer < diagonal_length):
-        replacement = nearest_non_terminal(CNF, matrix[pointer][pointer+diagonalKe], matrix[pointer+1][pointer+diagonalKe+1])
+        if (pointer+1 < len(matrix) and pointer+diagonalKe+1 < len(matrix)):
+            replacement = nearest_non_terminal(CNF, matrix[pointer][pointer+diagonalKe], matrix[pointer+1][pointer+diagonalKe+1])
         if (replacement != None):
             matrix[pointer][pointer+diagonalKe+1] = replacement
             pointer += 2
-        else:
+        elif (pointer+1 < len(matrix) and pointer+diagonalKe+1 < len(matrix)):
+            matrix[pointer][pointer+diagonalKe+1] = matrix[pointer][pointer+diagonalKe]
             pointer += 1
+        elif (pointer+1 == len(matrix) or pointer+diagonalKe+1 == len(matrix)):
+            matrix[pointer-1][pointer+diagonalKe] = matrix[pointer][pointer+diagonalKe]
+            pointer += 1
+        else:    
+            pointer += 1
+
 def cyk_parse(text,CNF):
     # PRODUKSI MATRIKS
     word_length = len(text)
-    cyk_matrix = np.zeros(word_length, word_length)
+    cyk_matrix = np.full((word_length, word_length), 'NULL')
     # ROW DIMULAI DARI 0 sampai len(text) - 1
     # COL DIMULAI DARI 1 sampai len(text)
     # CNF DALAM BENTUK DICTIONARY
@@ -60,6 +73,20 @@ def cyk_parse(text,CNF):
     # ITERASI DIAGONAL SAMPAI ATAS
     for diagonalKe in range(word_length-1):
         process_diagonal_matrix(CNF, cyk_matrix, diagonalKe)
+    beautiful_print(cyk_matrix)
+    return is_grammar_fulfilled_in_cyk_matrix('S', cyk_matrix)
 
-    return is_grammar_fulfilled_in_cyk_matrix('START', cyk_matrix)        
+# TEST
+CNF = {
+    'S': ('NP VP'),
+    'NP': ('Det N'),
+    'VP': ('V NP'),
+    'Det': ('the'),
+    'N': ('cat'),
+    'V': ('chased')
+}
+
+text = ['the', 'cat', 'chased', 'the', 'cat']
+
+print(cyk_parse(text, CNF))
 
