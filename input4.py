489b2ae3ae725bf9f318c6b191efd60d3645d02a
louisyeee <13520063@std.stei.itb.ac.id>
2021-11-24 23:15:51 +0700

    update cfg

---
+++ input4.py
@@ -0,0 +1,7 @@
+N = 5
+i = 1
+sum = 0
+
+while (i <= 5) :
+    sum += i
+    i += 1
---
+++ input5.py
@@ -0,0 +1,5 @@
+# di bawah ini adalah contoh output program yang salah
+# temukan letak kesalahannya
+
+for i in lines
+   print(i)
